ca427033706e9cfbc8dc28938f02e99744ce6eaa
MSUBAPARVATHI <subaparvathi2004@gmail.com>
2024-08-09 02:39:02 +0000

    alignment changes

--- my-web/src/components/Card.tsx
+++ my-web/src/components/Card.tsx
@@ -65,12 +65,6 @@ const Card: React.FC =()=>{
   {/* <div ></div> */}
   {/* className="divider divider-horizontal" */}
   {/* <div className="card flex w-full flex-col rounded-none lg:flex-row bg-white grid h-30 flex-grow place-items-center w-1/2">
-
-  <div className="w-1/2  ">
-    <div className="flex-grow w-100 p-100 mt-2 h-100 align-center">
-                <img src="/collection.webp"/>
-    </div>
-</div>
 </div> */}
 
 <div className='flex flex-grow w-1/2'>
@@ -123,9 +117,10 @@ const Card: React.FC =()=>{
     <div className="stat-value text-black">2.6M</div>
     <div className="stat-desc">21% more than last month</div>
   </div>
-
          </div>
+
          {/* 3 images container */}
+         <div>
 
             <div className="flex justify-between gap-5 w-full" id="slide1">
             <div className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 width-10 height-5 m-5 p-5 ... ">
@@ -140,28 +135,27 @@ const Card: React.FC =()=>{
 
             <div className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 width-5 height-5 m-5 p-5 ... ">
                 <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template/w_394,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1711428209166-2d42c0.jpeg"></img>
-            </div>
-        
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+    </div>
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide3" className="btn btn-circle">❮</a>
       <a href="#slide2" className="btn btn-circle">❯</a>
     </div>
-    </div>
-
-
+            </div>
+        
+        
         <div className="flex justify-between gap-5" id="slide2">
             <div className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 width-10 height-5 m-5 p-5 ... ">
-                <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template/w_394,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/supply/customer-app-supply/1715685371430-6b3871.jpeg"></img>
+                <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template/w_394,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1678454437383-aa4984.jpeg"></img>
                 
             </div>
 
             <div className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 width-5 height-5 m-5 p-5 ... ">
-                <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template/w_394,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/supply/customer-app-supply/1701759875987-8b654d.jpeg"></img>
+                <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template/w_394,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1678463309015-2b92d2.jpeg"></img>
             
             </div>
 
             <div className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 width-5 height-5 m-5 p-5 ... ">
-                <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template/w_394,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1711428209166-2d42c0.jpeg"></img>
+                <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template/w_394,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1723117999918-053994.jpeg"></img>
           </div>
             <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide1" className="btn btn-circle">❮</a>
@@ -190,52 +184,53 @@ const Card: React.FC =()=>{
     </div>
            
         </div>
-
+</div>
 
 
 
          {/* long images are added */}
 
          <div className="carousel w-50 mt-10 mb-10 ml-20 mr-30  w-50 h-50 m-10 justify-between">
-  <div id="slide1" className="carousel-item relative w-full">
+  <div id="slides1" className="carousel-item relative w-full">
     <img
       src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
       className="w-50" />
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide4" className="btn btn-circle">❮</a>
-      <a href="#slide2" className="btn btn-circle">❯</a>
+      <a href="#slides4" className="btn btn-circle">❮</a>
+      <a href="#slides2" className="btn btn-circle">❯</a>
     </div>
   </div>
-  <div id="slide2" className="carousel-item relative w-full">
+  <div id="slides2" className="carousel-item relative w-full">
     <img
       src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
       className="w-full" />
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide1" className="btn btn-circle">❮</a>
-      <a href="#slide3" className="btn btn-circle">❯</a>
+      <a href="#slides1" className="btn btn-circle">❮</a>
+      <a href="#slides3" className="btn btn-circle">❯</a>
     </div>
   </div>
-  <div id="slide3" className="carousel-item relative w-full">
+  <div id="slides3" className="carousel-item relative w-full">
     <img
       src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
       className="w-full" />
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide2" className="btn btn-circle">❮</a>
-      <a href="#slide4" className="btn btn-circle">❯</a>
+      <a href="#slides2" className="btn btn-circle">❮</a>
+      <a href="#slides4" className="btn btn-circle">❯</a>
     </div>
   </div>
-  <div id="slide4" className="carousel-item relative w-full">
+  <div id="slides4" className="carousel-item relative w-full">
     <img
       src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
       className="w-full" />
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide3" className="btn btn-circle">❮</a>
-      <a href="#slide1" className="btn btn-circle">❯</a>
+      <a href="#slides3" className="btn btn-circle">❮</a>
+      <a href="#slides1" className="btn btn-circle">❯</a>
     </div>
   </div>
 </div>
 
 </div>
+
 // another card
 
 
